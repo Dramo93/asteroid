83dd57ffeff931b913a1c3bf1d9999efb95bc1c0
Dramo <giorgiodramo@gmail.com>
2024-12-27 12:07:08 +0100

    added player with turning logic

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    x = SCREEN_WIDTH/2
+    y = SCREEN_HEIGHT/2
+    play = Player(x,y)
+    dt = 0
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -12,7 +18,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0,0,0))
+        play.draw(screen)
+        play.update(dt)
         pygame.display.flip()
+
+        time_passed = clock.tick(60)
+        dt = time_passed /1000
 
 if __name__ == "__main__":
     main()
